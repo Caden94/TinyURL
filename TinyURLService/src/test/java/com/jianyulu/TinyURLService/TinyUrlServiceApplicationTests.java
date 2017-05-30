4cc20ff0593c04299985a3284414891d715e6cd2
LuJianyu <lujianyu@LuJianyutekiMacBook-Pro.local>
2017-05-29 22:25:21 -0400

    Start TinyURL Service Project with Spring

---
+++ TinyURLService/src/test/java/com/jianyulu/TinyURLService/TinyUrlServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jianyulu.TinyURLService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TinyUrlServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
