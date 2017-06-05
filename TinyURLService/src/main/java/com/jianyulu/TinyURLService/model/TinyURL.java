edc644fbb4132e963e489074f96a39d142453ad0
LuJianyu <lujianyu@LuJianyutekiMacBook-Pro.local>
2017-06-04 22:36:27 -0400

    use lomkok auto generate setter, getter and constructer

--- TinyURLService/src/main/java/com/jianyulu/TinyURLService/model/TinyURL.java
+++ TinyURLService/src/main/java/com/jianyulu/TinyURLService/model/TinyURL.java
@@ -1,26 +1,20 @@
 package com.jianyulu.TinyURLService.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Created by lujianyu on 6/4/17.
  */
+@Data
+@NoArgsConstructor
 public class TinyURL {
+
     private String shortURL;
     private String longURL;
-
-    public TinyURL() {
-
-    }
 
     public TinyURL(String shortURL, String longURL) {
         this.shortURL = shortURL;
         this.longURL = longURL;
-    }
-
-    public String getShortURL() {
-        return shortURL;
-    }
-
-    public String getLongURL() {
-        return longURL;
     }
 }
